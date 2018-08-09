7b2a49e2ba906d5a639a2113901903f040a25780
富田地広 <bwlmaniatomy@gmail.com>
2018-08-09 17:19:11 +0900

    Add before_action to stores_controller.rb

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -1,4 +1,6 @@
 class StoresController < ApplicationController
+  before_action :move_to_index, except: :index
+
   def index
     @stores = Store.order("created_at DESC").page(params[:page]).per(5)
   end
@@ -14,5 +16,9 @@ class StoresController < ApplicationController
   private
   def store_params
     params.permit(:store_name, :c_card, :e_money)
+  end
+
+  def move_to_index
+    redirect_to action: :index unless user_signed_in?
   end
 end
