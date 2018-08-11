850ba87958e088d53677affc64d3064ff48911b1
富田地広 <bwlmaniatomy@gmail.com>
2018-08-11 21:48:13 +0900

    Add search button

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'stores#index'
-  resources :stores, only: [:new, :create, :edit, :update, :destroy]
-  collection do
-    get 'search'
+  resources :stores, only: [:new, :create, :edit, :update, :destroy] do
+    collection do
+      get 'search'
+    end
   end
 end
