a221d0318c625a63259054a45d913e7fcfe2547d
富田地広 <bwlmaniatomy@gmail.com>
2018-08-11 21:25:02 +0900

    add get search to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'stores#index'
   resources :stores, only: [:new, :create, :edit, :update, :destroy]
+  collection do
+    get 'search'
+  end
 end
